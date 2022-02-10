1047cc13221efefe0b5b0a1ba566e3e93b99d45a
NicoleH87 <nicoleh87@nycstudents.net>
2022-02-10 00:15:55 -0500

    U7L5 FRQ

--- src/RepairSchedule.java
+++ src/RepairSchedule.java
@@ -1,69 +1,78 @@
 import java.util.ArrayList;
 
-public class RepairSchedule {
-    /**
-     * Each element represents a repair by an individual mechanic in a bay.
-     */
+public class RepairSchedule
+{
+    /** Each element represents a repair by an individual mechanic in a bay. */
     private ArrayList<CarRepair> schedule;
 
-    /**
-     * Number of mechanics available in this schedule.
-     */
+    /** Number of mechanics available in this schedule. */
     private int numberOfMechanics;
 
-    /**
-     * Constructs a RepairSchedule object.
+    /** Constructs a RepairSchedule object.
      * Precondition: n >= 0
      */
-    public RepairSchedule(int n) {
+    public RepairSchedule(int n)
+    {
         schedule = new ArrayList<CarRepair>();
         numberOfMechanics = n;
     }
 
-    public ArrayList<CarRepair> getSchedule() {
+    public ArrayList<CarRepair> getSchedule()
+    {
         return schedule;
     }
 
-    /**
-     * Attempts to schedule a repair by a given mechanic in a given bay as described in part (a).
+    /** Attempts to schedule a repair by a given mechanic in a given bay as described in part (a).
      * Precondition: 0 <= m < numberOfMechanics and b >= 0
      */
-    public boolean addRepair(int m, int b) {
-        for (int i = 0; i < schedule.size(); i++) {
-            if (schedule.get(i).getMechanicNum() == m || schedule.get(i).getBayNum() == b) {
+    public boolean addRepair(int m, int b)
+    {
+        for ( CarRepair c : schedule)
+        {
+            if (c.getMechanicNum() == m || c.getBayNum() == b)
+            {
                 return false;
             }
         }
+        CarRepair repair = new CarRepair(m,b);
+        schedule.add(repair);
         return true;
     }
 
-    /**
-     * Returns an ArrayList containing the mechanic identifiers of all available mechanics,
+    /** Returns an ArrayList containing the mechanic identifiers of all available mechanics,
      * as described in part (b).
      */
-    public ArrayList<Integer> availableMechanics() {
-        ArrayList<Integer> avail = new ArrayList<Integer>();
-        for (int x = 0; x < numberOfMechanics; x++) {
-            for (int i = 0; i < schedule.size(); i++) {
-                if (schedule.get(i).getMechanicNum() == x) {
-                    avail.add(x);
+    public ArrayList<Integer> availableMechanics()
+    {
+        ArrayList<Integer> m = new ArrayList<Integer>();
+        for (int i = 0; i < numberOfMechanics; i++)
+        {
+            boolean f = true;
+            for (CarRepair x : schedule)
+            {
+                if (x.getMechanicNum() == i)
+                {
+                    f = false;
                 }
             }
+            if (f == true)
+            {
+                m.add(i);
+            }
         }
-        return avail;
+        return m;
     }
 
-    /**
-     * Removes an element from schedule when a repair is complete.
-     */
-    public void carOut(int b) {
-        for (int i = 0; i < schedule.size(); i++) {
+    /** Removes an element from schedule when a repair is complete. */
+    public void carOut(int b)
+    {
+        for (int i = 0; i < schedule.size(); i++)
+        {
             CarRepair carAtIdx = schedule.get(i);
-            if (carAtIdx.getBayNum() == b) {
+            if (carAtIdx.getBayNum() == b)
+            {
                 schedule.remove(i);
             }
         }
     }
-
 }
-
